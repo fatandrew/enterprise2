e7565d470a1f2025bad3d57ad4b834119c9fca1d
Andrew Danylenko <wdanylenko@gmail.com>
2016-06-13 22:12:14 +0300

    add class SomeWork

---
+++ src/SomeWork.java
@@ -0,0 +1,44 @@
+import com.sun.javafx.tk.Toolkit;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by User on 13.06.2016.
+ */
+public class SomeWork implements Executor {
+    @Override
+    public void addTask(Toolkit.Task task) {
+
+    }
+
+    @Override
+    public void addTask(Object task, Object validator) {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public Object getResult() {
+        return null;
+    }
+
+    @Override
+    public List getValidResults() {
+        return null;
+    }
+
+    @Override
+    public List getInvalidResults() {
+        return null;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
